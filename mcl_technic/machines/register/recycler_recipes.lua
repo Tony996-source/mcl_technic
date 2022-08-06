7e6cc6613565056ef1edb9a59a68429bab4f1278
Tony996-source <62044609+Tony996-source@users.noreply.github.com>
2022-08-06 18:08:32 +0100

    update

--- mcl_technic/machines/register/recycler_recipes.lua
+++ mcl_technic/machines/register/recycler_recipes.lua
@@ -12,21 +12,21 @@ end
 
 -- register recipes
 local recipes = {
-	{"mcl_technic:hv_alloy_furnace","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_battery_box","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_compressor","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_electric_furnace","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_extractor","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_freezer","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_generator","mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_grinder", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_quarry", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_recycler", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:hv_sawmill", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:solar_array_hv", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:tool_workshop_hv", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:chainsaw_hv", "mcl_nether:netherite_scrap"},
-	{"mcl_technic:vacuum_hv", "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_alloy_furnace",    "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_battery_box",      "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_compressor",       "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_electric_furnace", "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_extractor",        "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_freezer",          "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_generator",        "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_grinder",          "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_quarry",           "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_recycler",         "mcl_nether:netherite_scrap"},
+	{"mcl_technic:hv_sawmill",          "mcl_nether:netherite_scrap"},
+	{"mcl_technic:solar_array_hv",      "mcl_nether:netherite_scrap"},
+	{"mcl_technic:tool_workshop_hv",    "mcl_nether:netherite_scrap"},
+	{"mcl_technic:chainsaw_hv",         "mcl_nether:netherite_scrap"},
+	{"mcl_technic:vacuum_hv",           "mcl_nether:netherite_scrap"},
 }
 
 
