f4085e76c4ec74307f992c5af0d9503cc423e1a4
Tony996-source <62044609+Tony996-source@users.noreply.github.com>
2022-08-07 17:33:05 +0100

    update ore generation

--- mcl_technic_worldgen/mapgen.lua
+++ mcl_technic_worldgen/mapgen.lua
@@ -9,16 +9,6 @@ minetest.register_ore({
 	clust_num_ores = 5,
 	clust_size     = 3,
 	y_min          = mcl_vars.mg_overworld_min,
-	y_max          = mcl_worlds.layer_to_y(39),
-})
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "mcl_copper:stone_with_copper",
-	wherein         = stonelike,
-	clust_scarcity = 1660,
-	clust_num_ores = 4,
-	clust_size     = 3,
-	y_min          = mcl_worlds.layer_to_y(40),
 	y_max          = mcl_worlds.layer_to_y(63),
 })
 
@@ -31,20 +21,10 @@ minetest.register_ore({
 		clust_scarcity = 600*3,
 		clust_num_ores = 3,
 		clust_size     = 2,
-		y_min          = mcl_worlds.layer_to_y(81),
+		y_min          = mcl_vars.mg_overworld_min,
 		y_max          = mcl_worlds.layer_to_y(128),
 	})
-minetest.register_ore({
-		ore_type       = "scatter",
-		ore            = "mcl_core:stone_with_coal",
-		wherein         = stonelike,
-		clust_scarcity = 550*3,
-		clust_num_ores = 4,
-		clust_size     = 3,
-		y_min          = mcl_worlds.layer_to_y(81),
-		y_max          = mcl_worlds.layer_to_y(128),
-	})
-	
+
 -- Iron
 
 minetest.register_ore({
@@ -55,7 +35,7 @@ minetest.register_ore({
 		clust_num_ores = 5,
 		clust_size     = 3,
 		y_min          = mcl_vars.mg_overworld_min,
-		y_max          = mcl_worlds.layer_to_y(39),
+		y_max          = mcl_worlds.layer_to_y(63),
 	})
 minetest.register_ore({
 		ore_type       = "scatter",
@@ -77,8 +57,8 @@ minetest.register_ore({
 		clust_scarcity = 13000,
 		clust_num_ores = 4,
 		clust_size     = 2,
-		y_min          = mcl_worlds.layer_to_y(31),
-		y_max          = mcl_worlds.layer_to_y(33),
+		y_min          = mcl_vars.mg_overworld_min,
+		y_max          = mcl_worlds.layer_to_y(63),
 	})
 	
 minetest.register_ore({
@@ -101,8 +81,8 @@ minetest.register_ore({
 		clust_scarcity = 20000,
 		clust_num_ores = 1,
 		clust_size     = 1,
-		y_min          = mcl_worlds.layer_to_y(13),
-		y_max          = mcl_worlds.layer_to_y(15),
+		y_min          = mcl_vars.mg_overworld_min,
+		y_max          = mcl_worlds.layer_to_y(63),
 	})
 minetest.register_ore({
 		ore_type       = "scatter",
